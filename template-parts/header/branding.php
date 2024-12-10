c275032621819465a879ed0bc61d9172adfb9f62
robruiz <rob.c.ruiz@gmail.com>
2024-12-10 00:48:20 -0600

    Styling for header and main background colors also added svg logo

---
+++ template-parts/header/branding.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template part for displaying the header branding
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+?>
+
+<div class="site-branding">
+	<a href="<?php echo get_site_url(); ?>" class="custom-logo-link" rel="home" aria-current="page">
+		<?php get_template_part( 'template-parts/header/logo' ); ?>
+	</a>
+
+	<?php
+	if ( is_front_page() && is_home() ) {
+		?>
+		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<?php
+	} else {
+		?>
+		<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+		<?php
+	}
+	?>
+
+	<?php
+	$wp_rig_description = get_bloginfo( 'description', 'display' );
+	if ( $wp_rig_description || is_customize_preview() ) {
+		?>
+		<p class="site-description">
+			<?php echo $wp_rig_description; /* phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped */ ?>
+		</p>
+		<?php
+	}
+	?>
+</div><!-- .site-branding -->
