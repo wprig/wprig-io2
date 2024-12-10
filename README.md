b2b3c13d754e72abec44d52366090c351f168511
robruiz <rob.c.ruiz@gmail.com>
2024-12-09 22:47:38 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# wprig-io2
-Version 2 of the WP Rig IO website theme
+# WPRig.io Theme
+[![License: GPL](https://img.shields.io/aur/license/yaourt.svg)](https://www.gnu.org/licenses/gpl-3.0.en.html)
+
+Progressive theme for the [WPRig.io](https://wprig.io) website.
+
+Based on [WP Rig](https://github.com/wprig/wprig).
+
+## License
+WP Rig is released under [GNU General Public License v3.0](https://github.com/wprig/wprig/blob/master/LICENSE).
---
+++ template-parts/content-grid.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template part for displaying individual course materials in the course index.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package wprig
+ */
+
+?>
+<article id="post-<?php the_ID(); ?>" class="course-item">
+	<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+		<div class="course-wrapper">
+			<div class="video-thumb">
+				<?php the_post_thumbnail( 'video-thumb' ); ?>
+			</div><!-- .post-thumbnail -->
+			<?php the_title( '<h2 class="entry-title" rel="bookmark">', '</h2>' ); ?>
+		</div>
+		<div class="movie-cta">Watch this lesson.</div>
+	</a>
+</article>
+
