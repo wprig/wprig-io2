7b9d9b072018cc16d8aa1dc9bf679f7832923965
robruiz <rob.c.ruiz@gmail.com>
2024-12-30 20:48:34 -0600

    better micro animation in main nav for sub menu present

--- template-parts/content/entry_header.php
+++ template-parts/content/entry_header.php
@@ -6,7 +6,7 @@
  */
 
 namespace WP_Rig\WP_Rig;
-
+global $post;
 ?>
 
 <header class="entry-header<?php echo (has_post_thumbnail() ? ' has-thumb' : '')?>">
@@ -20,8 +20,9 @@ namespace WP_Rig\WP_Rig;
 	<div class="entry-top-details">
 	<?php
 	get_template_part( 'template-parts/content/entry_title', get_post_type() );
-
-	get_template_part( 'template-parts/content/entry_meta', get_post_type() );
+	if($post->post_type !== 'page'){
+		get_template_part( 'template-parts/content/entry_meta', get_post_type() );
+	}
 	?>
 	</div>
 </header><!-- .entry-header -->
